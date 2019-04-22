cc5202e2925bb914d81f81462615e67bdf6f6586
lele-mobile <emanuele7804@gmail.com>
2019-04-22 18:01:58 +0200

    version 1.0.1

--- app/src/main/java/com/example/lele/officina/Controllo.kt
+++ app/src/main/java/com/example/lele/officina/Controllo.kt
@@ -5,18 +5,25 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.media.Image
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.print.PrintJob
+import android.printservice.PrintDocument
+import android.provider.DocumentsContract
 import android.support.v4.content.FileProvider
 import android.support.v4.print.PrintHelper
 import android.text.Editable
 import android.util.DisplayMetrics
 import android.view.View
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_controllo.*
 import kotlinx.android.synthetic.main.activity_preventivo.*
+import kotlinx.android.synthetic.main.activity_preventivo.targaTitP
 import kotlinx.android.synthetic.main.content_preventivo.*
+import org.w3c.dom.Document
 import java.io.File
 import java.io.FileOutputStream
 import java.text.SimpleDateFormat
@@ -67,6 +74,44 @@ class Controllo : AppCompatActivity() {
         val buttonBack = findViewById(R.id.buttonBack) as ImageButton
         val buttonPrint = findViewById(R.id.printButton) as ImageButton
         val mailButton = findViewById(R.id.mailButton) as ImageButton
+        val nextPage = findViewById(R.id.nextPage) as ImageButton
+        val prevPage = findViewById(R.id.prevPage) as ImageButton
+
+        nextPage.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+
+               numPage.text = "2"
+                content_controllo_print.visibility = View.INVISIBLE
+                    content_controllo_print2.visibility = View.VISIBLE
+                nextPage.visibility = View.INVISIBLE
+                    prevPage.visibility = View.VISIBLE
+
+
+
+
+            }
+        })
+
+
+            prevPage.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+
+
+                    numPage.text = "1"
+                    content_controllo_print.visibility = View.VISIBLE
+                    content_controllo_print2.visibility = View.INVISIBLE
+                    nextPage.visibility = View.VISIBLE
+                    prevPage.visibility = View.INVISIBLE
+
+
+
+
+                }
+            })
+
+
+
 
         buttonPrint.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
@@ -76,7 +121,20 @@ class Controllo : AppCompatActivity() {
                     windowManager.defaultDisplay.getMetrics(displayMetrics)
 
                 val bitmap = loadBitmapFromView(findViewById(R.id.content_controllo_print), displayMetrics.widthPixels, displayMetrics.heightPixels)
-                    doPhotoPrint(bitmap)
+
+                content_controllo_print.visibility = View.INVISIBLE
+                     content_controllo_print2.visibility = View.VISIBLE
+
+                val bitmap2 = loadBitmapFromView(findViewById(R.id.content_controllo_print2), displayMetrics.widthPixels, displayMetrics.heightPixels)
+
+
+
+                    doPhotoPrint(bitmap, bitmap2)
+                numPage.text = "2"
+                content_controllo_print.visibility = View.INVISIBLE
+                content_controllo_print2.visibility = View.VISIBLE
+                nextPage.visibility = View.INVISIBLE
+                prevPage.visibility = View.VISIBLE
 
 
 
@@ -91,7 +149,19 @@ class Controllo : AppCompatActivity() {
                     windowManager.defaultDisplay.getMetrics(displayMetrics)
 
                 val bitmap = loadBitmapFromView(findViewById(R.id.content_controllo_print), displayMetrics.widthPixels, displayMetrics.heightPixels)
-                    saveImage(bitmap)
+
+                content_controllo_print.visibility = View.INVISIBLE
+                content_controllo_print2.visibility = View.VISIBLE
+
+                val bitmap2 = loadBitmapFromView(findViewById(R.id.content_controllo_print2), displayMetrics.widthPixels, displayMetrics.heightPixels)
+
+                saveImage(bitmap, bitmap2, "condividi")
+
+                numPage.text = "2"
+                    content_controllo_print.visibility = View.INVISIBLE
+                        content_controllo_print2.visibility = View.VISIBLE
+                nextPage.visibility = View.INVISIBLE
+                    prevPage.visibility = View.VISIBLE
 
 
 
@@ -107,28 +177,45 @@ class Controllo : AppCompatActivity() {
 
     }
 
-    private fun doPhotoPrint(bitmap: Bitmap) {
+
+    private fun doPhotoPrint(bitmap: Bitmap, bitmap2: Bitmap) {
+
         val photoPrinter = PrintHelper(this)
-        photoPrinter.scaleMode = PrintHelper.SCALE_MODE_FIT
-        photoPrinter.printBitmap("test print", bitmap)
+            photoPrinter.scaleMode = PrintHelper.SCALE_MODE_FIT
+                photoPrinter.printBitmap("test print", bitmap) ; photoPrinter.printBitmap("test print", bitmap2)
+
     }
 
 
-    fun saveImage(bitmap: Bitmap) {
+    fun saveImage(bitmap: Bitmap, bitmap2: Bitmap, stringa: String) {
         val root = Environment.getExternalStorageDirectory().toString()
-        val myDir = File(root + "/offcinaPrint")
-        myDir.mkdirs()
 
-        val fname = "PrintContr.jpg"
+        val myDir = File(root + "/offcinaPrint")
+            myDir.mkdirs()
+
+        val fname = "PrintControlloP1.jpg"
+            val fname2 = "PrintControlloP2.jpg"
+
         val file = File(myDir, fname)
+            val file2 = File(myDir, fname2)
+
         //  Log.i(TAG, "" + file);
         if (file.exists())
+
             file.delete()
+                file2.delete()
+
         try {
             val out = FileOutputStream(file)
+                val out2 = FileOutputStream(file2)
+
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+                bitmap2.compress(Bitmap.CompressFormat.JPEG, 100, out2)
+
             out.flush()
-            out.close()
+                out.close()
+                    out2.flush()
+                        out2.close()
 
 
 
@@ -141,21 +228,40 @@ class Controllo : AppCompatActivity() {
             "com.example.homefolder.example.provider", //(use your app signature + ".provider" )
             file
         )
+        val imageUri2 = FileProvider.getUriForFile(
+            this@Controllo,
+            "com.example.homefolder.example.provider", //(use your app signature + ".provider" )
+            file2
+        )
 
-        shareImage(imageUri)
+        if(stringa == "stampa"){
+
+
+        }else if (stringa== "condividi"){
+
+            shareImage(imageUri, imageUri2)
+
+        }
+
+
 
 
     }
 
-    private fun shareImage(file: Uri) {
+    private fun shareImage(file: Uri, file2: Uri) {
 
         val intent = Intent()
-        intent.action = Intent.ACTION_SEND
-        intent.type = "image/*"
+            intent.action = Intent.ACTION_SEND_MULTIPLE
+                intent.type = "image/*"
+
+        var files = ArrayList<Uri>()
+            files.add(file)
+                files.add(file2)
 
         intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "")
-        intent.putExtra(android.content.Intent.EXTRA_TEXT, "")
-        intent.putExtra(Intent.EXTRA_STREAM, file)
+            intent.putExtra(android.content.Intent.EXTRA_TEXT, "")
+                intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, files)
+
         try {
             startActivity(Intent.createChooser(intent, "Share Screenshot"))
         } catch (e: ActivityNotFoundException) {
