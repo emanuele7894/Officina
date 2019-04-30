0a94cd9d39ec9d7b86823716ae5c38765a393b1e
lele-mobile <emanuele7804@gmail.com>
2019-04-30 18:39:21 +0200

    version 1.0.1

--- app/src/main/java/com/example/lele/officina/ListaClienti.kt
+++ app/src/main/java/com/example/lele/officina/ListaClienti.kt
@@ -3,13 +3,16 @@ package com.example.lele.officina
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ImageButton
-import android.widget.ListView
+import android.widget.*
 import com.example.lele.officina.adapter.offiListAdapter
 import com.example.lele.officina.data.officinaDati
 import com.google.firebase.database.*
+import com.mancj.materialsearchbar.MaterialSearchBar
+import kotlinx.android.synthetic.main.activity_lista_clienti.*
+import java.util.*
 
 class ListaClienti : AppCompatActivity() {
 
@@ -25,28 +28,49 @@ class ListaClienti : AppCompatActivity() {
         setContentView(R.layout.activity_lista_clienti)
 
 
+        listView = findViewById(R.id.listView) as ListView
+        val buttonBackton = findViewById(R.id.buttonBack) as ImageButton
+
+        val searchBar = findViewById(R.id.searcBar) as MaterialSearchBar
+            searchBar.setHint("Cerca cliente...")
+                searchBar.setSpeechMode(true)
+
         dati = mutableListOf()
         ref = FirebaseDatabase.getInstance().getReference("officinaData")
 
-
+        var datiRicerca = arrayOf("")
+        var datie = ""
 
         ref.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
+
             override fun onDataChange(p0: DataSnapshot) {
 
                 if(p0.exists()){
+
+
+
 
                    for(h in p0.children){
                        val dat = h.getValue(officinaDati::class.java)
                        dati.add(dat!!)
 
+
+                       datiRicerca.set(0, dat.idName)
+
+                       Toast.makeText(applicationContext," ${h.value.toString()}  ",Toast.LENGTH_SHORT).show()
+
+
+
                    }
 
                     val adapter = offiListAdapter(applicationContext, R.layout.offic_list_row, dati)
-                        listView.adapter = adapter
+                       listView.adapter = adapter
+
+
 
 
                 }
@@ -58,11 +82,11 @@ class ListaClienti : AppCompatActivity() {
 
 
 
+
+
         var k1: Intent = Intent(this@ListaClienti, officina_cli::class.java)
 
 
-        listView = findViewById(R.id.listView) as ListView
-        val buttonBackton = findViewById(R.id.buttonBack) as ImageButton
 
         buttonBackton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
@@ -119,7 +143,39 @@ class ListaClienti : AppCompatActivity() {
         }
 
 
+
+
+        val adapter2 = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datiRicerca)
+        listView2.adapter = adapter2
+        listView2.visibility = View.INVISIBLE
+
+        //SEARCHBAR TEXT CHANGE LISTENER
+        searchBar.addTextChangeListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+
+            }
+
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                //SEARCH FILTER
+                if (searchBar.text == ""){
+                    listView2.visibility = View.INVISIBLE
+                }else {
+                    listView2.visibility = View.VISIBLE
+                }
+
+                adapter2.getFilter().filter(charSequence)
+
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+
+            }
+        })
+
+
     }
+
+
 
 
     override fun onRestart() {
