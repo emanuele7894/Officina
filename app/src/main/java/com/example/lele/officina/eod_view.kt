2b6edf71e7805c78d755f296c9fe2ed8323d8593
lele-mobile <emanuele7804@gmail.com>
2019-05-04 11:55:54 +0200

    version 1.0.1

--- app/src/main/java/com/example/lele/officina/eod_view.kt
+++ app/src/main/java/com/example/lele/officina/eod_view.kt
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_eod_view.*
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+
+
 
 class eod_view : AppCompatActivity() {
 
@@ -17,6 +22,48 @@ class eod_view : AppCompatActivity() {
             }
         })
 
+        editTextCode.addTextChangedListener(object : TextWatcher {
 
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+                if (count > 0){
+
+                    editTextCode.setInputType(InputType.TYPE_CLASS_NUMBER)
+
+
+                }
+
+                if(editTextCode.text.toString() == ""){
+
+                    editTextCode.setInputType(InputType.TYPE_CLASS_TEXT)
+                        editTextCode.setInputType(InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS)
+
+
+
+                }
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
+
+
+
+        okButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+
+
+            }
+        })
     }
 }
