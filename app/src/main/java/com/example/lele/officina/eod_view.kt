c7f49def048a50dd7681a921b3ad3e297b80689a
lele-mobile <emanuele7804@gmail.com>
2019-05-06 15:12:36 +0200

    version 1.0.1

--- app/src/main/java/com/example/lele/officina/eod_view.kt
+++ app/src/main/java/com/example/lele/officina/eod_view.kt
@@ -1,8 +1,10 @@
 package com.example.lele.officina
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import kotlinx.android.synthetic.main.activity_eod_view.*
 import android.text.Editable
@@ -11,8 +13,15 @@ import android.text.TextWatcher
 import android.text.method.TextKeyListener
 import android.view.KeyEvent
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.ListView
+import com.example.lele.officina.adapter.CustomAdapter
+import com.example.lele.officina.data.ImageModel
 import com.example.lele.officina.data.officinaDati
 import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_eod_view.buttonBack
+import kotlinx.android.synthetic.main.activity_interventi_view.view.*
+import kotlinx.android.synthetic.main.activity_lista_clienti.*
 
 
 class eod_view : AppCompatActivity() {
@@ -20,13 +29,35 @@ class eod_view : AppCompatActivity() {
 
     lateinit var  ref: DatabaseReference
     var keyC = true
+    var openL = true
     var dati = hashMapOf<String,String>()
     var modello = "Fiat"
+
+
+    internal lateinit var lv: ListView
+    private var customeAdapter: CustomAdapter? = null
+    private var imageModelArrayList: ArrayList<ImageModel>? = null
+    private val myImageList = intArrayOf(R.mipmap.alfa_romeo,R.mipmap.altri_veicoli,R.mipmap.audi,R.mipmap.bmw, R.mipmap.citroen,R.mipmap.dacia,R.mipmap.fiat,R.mipmap.ford,R.mipmap.hyundai,R.mipmap.iveco,
+        R.mipmap.lancia,R.mipmap.mercedes,R.mipmap.miniold,R.mipmap.nissan,R.mipmap.opel,R.mipmap.peugeot,R.mipmap.renault,R.mipmap.rover,R.mipmap.saab,R.mipmap.seat,
+        R.mipmap.skoda,R.mipmap.smart,R.mipmap.volkswagen2,R.mipmap.volvo)
+    private val myImageNameList = arrayOf("Alfa Romeo","ALtri veicoli", "Audi","Bmw","Citroen","Dacia","Fiat","Ford",
+        "Hyundai", "Iveco", "Lancia", "Mercedes-benz", "Mini", "Nissan", "Opel", "Peugeot", "Renault", "Rover", "Saab",
+        "Seat", "Skoda", "Smart", "Volkswagen", "Volvo")
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_eod_view)
 
+        lv = findViewById(R.id.listView) as ListView
+
+        imageModelArrayList = populateList()
+        customeAdapter = CustomAdapter(this, imageModelArrayList!!)
+        lv!!.adapter = customeAdapter
+
+
+        //Carica Dati array
         fun loadRef(){
             ref = FirebaseDatabase.getInstance().getReference("obd").child(modello)
 
@@ -61,16 +92,217 @@ class eod_view : AppCompatActivity() {
         selMarca.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
 
-                dati.clear()
-                    modello = "Bmw"
 
-                loadRef()
+                if (openL == true){
+                    //Lista Visibile
+
+                    lv.visibility = View.VISIBLE
+
+                    val listVisible = ObjectAnimator.ofFloat(lv, View.ALPHA,  1.0f)
+                        listVisible.duration = 500
+                            listVisible.start()
+                    openL = false
+                        selMarca.alpha = 0.4f
+
+
+
+                }else {
+
+
+
+
+                    val listVisible = ObjectAnimator.ofFloat(lv, View.ALPHA,  0.0f)
+                        listVisible.duration = 500
+                            listVisible.start()
+
+
+
+                    Handler().postDelayed({
+                        lv.visibility = View.INVISIBLE
+                    }, 500)
+                    openL = true
+                        selMarca.alpha = 1.0f
+
+
+                }
+
 
 
             }
         })
 
+        lv.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, index, l ->
 
+
+            dati.clear()
+
+            if(index == 0){
+
+                    modello = "Fiat"
+                        marcaText.text = myImageNameList[index]
+                            selMarca.setImageResource(R.mipmap.alfa_romeo)
+
+            }else if (index == 1){
+
+                    modello = "Standard"
+                        marcaText.text = myImageNameList[index]
+                            selMarca.setImageResource(R.mipmap.altri_veicoli)
+
+            }else if (index == 2){
+
+                modello = "Vag"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.audi)
+
+            }else if (index == 3){
+
+                modello = "Bmw"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.bmw)
+
+            }else if (index == 4){
+
+                modello = "PSA"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.citroen)
+
+            }else if (index == 5){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.dacia)
+
+            }else if (index == 6){
+
+                modello = "Fiat"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.fiat)
+
+            }else if (index == 7){
+
+                modello = "Ford"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.ford)
+
+            }else if (index == 8){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.hyundai)
+
+            }else if (index == 9){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.iveco)
+
+            }else if (index == 10){
+
+                modello = "Fiat"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.lancia)
+
+            }else if (index == 11){
+
+                modello = "Mercedes"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.mercedes)
+
+            }else if (index == 12){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.miniold)
+
+            }else if (index == 13){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.nissan)
+
+            }else if (index == 14){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.opel)
+
+            }else if (index == 15){
+
+                modello = "PSA"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.peugeot)
+
+            }else if (index == 16){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.renault)
+
+            }else if (index == 17){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.rover)
+
+            }else if (index == 18){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.saab)
+
+            }else if (index == 19){
+
+                modello = "Vag"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.seat)
+
+            }else if (index == 20){
+
+                modello = "Vag"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.skoda)
+
+            }else if (index == 21){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.smart)
+
+            }else if (index == 22){
+
+                modello = "Vag"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.volkswagen2)
+
+            }else if (index == 23){
+
+                modello = "Standard"
+                    marcaText.text = myImageNameList[index]
+                        selMarca.setImageResource(R.mipmap.volvo)
+
+            }
+
+
+            loadRef()
+                openL = true
+                    selMarca.alpha = 1.0f
+
+
+
+            //chiusura Lista
+            val listVisible = ObjectAnimator.ofFloat(lv, View.ALPHA,  0.0f)
+                listVisible.duration = 500
+                    listVisible.start()
+
+
+            Handler().postDelayed({
+
+                lv.visibility = View.INVISIBLE
+
+            }, 500)
+
+
+        }
 
 
 
@@ -186,6 +418,21 @@ class eod_view : AppCompatActivity() {
 
 
 
+    }
+
+
+    private fun populateList(): ArrayList<ImageModel> {
+
+        val list = ArrayList<ImageModel>()
+
+        for (i in 0..myImageNameList.count() - 1) {
+            val imageModel = ImageModel()
+            imageModel.setNames(myImageNameList[i])
+            imageModel.setImage_drawables(myImageList[i])
+            list.add(imageModel)
+        }
+
+        return list
     }
 
 
