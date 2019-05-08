f4137c6ff58d16e9454aa8b310200cb1af0be6fc
lele-mobile <emanuele7804@gmail.com>
2019-05-08 18:43:33 +0200

    version 1.0.1

--- app/src/main/java/com/example/lele/officina/datiVeicolo.kt
+++ app/src/main/java/com/example/lele/officina/datiVeicolo.kt
@@ -1,7 +1,9 @@
 package com.example.lele.officina
 
+import android.animation.ObjectAnimator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import kotlinx.android.synthetic.main.activity_dati_veicolo.*
 
@@ -11,6 +13,11 @@ class datiVeicolo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dati_veicolo)
 
+        codicePannel.visibility = View.INVISIBLE
+            codicePannel.alpha = 0.0f
+
+
+
 
         buttonBack.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
@@ -18,7 +25,48 @@ class datiVeicolo : AppCompatActivity() {
             }
         })
 
+        addCodic.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                codicePannel.visibility = View.VISIBLE
+
+
+                val listVisible = ObjectAnimator.ofFloat(codicePannel, View.ALPHA,  1.0f)
+                        listVisible.duration = 500
+                             listVisible.start()
+
+
+            }
+        })
+
+        fun invisiblePannel(){
+
+
+
+            val listVisible = ObjectAnimator.ofFloat(codicePannel, View.ALPHA,  0.0f)
+                listVisible.duration = 500
+                    listVisible.start()
+
+
+
+            Handler().postDelayed({
+
+                codicePannel.visibility = View.INVISIBLE
+
+            }, 500)
+
+        }
+
+        closePannel.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                invisiblePannel()
+            }
+        })
+
+
+
     }
+
+
 
 
 
